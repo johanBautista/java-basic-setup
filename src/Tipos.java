732a651a3eaa8bc2df63501de349b8a81ec9ec97
Johan Bautista <jsbparra@gmail.com>
2023-05-01 22:03:02 +0200

    tipos

---
+++ src/Tipos.java
@@ -0,0 +1,28 @@
+public class Tipos {
+    public static void main(String[] args) {
+        // Enteros
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4l;
+
+        // Punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 4.99d;
+
+        //Caracter
+        char caracter1 = 'b';
+
+        //Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Cadenas texto
+        String nombre = "Joe";
+        String apellido = "Doe";
+
+        //Tipos envoltorio
+        Integer numero = null;
+        Long numero2 = 2L;
+    }
+}
---
+++ src/com/johan_dev/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.johan_dev;
+
+public class HelloWorld {
+    /**
+     * Documentation
+     * @param args
+     */
+    public static void main(String[] args){
+// commentary style 1
+        System.out.println("Commentary  test!");
+        /*
+        * commentary style 2
+        * */
+    }
+}
