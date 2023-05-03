1a1666a2083451d2521f982f4b422b7c23b2bb3e
Johan Bautista <jsbparra@gmail.com>
2023-05-03 20:15:09 +0200

    operadores

---
+++ src/Operadores.java
@@ -0,0 +1,42 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Operadores aritmeticos
+        int number1 = 1;
+        int number2 = 2;
+
+        int result1 = number1 + number2;
+        int result2 = number1 - number2;
+        int result3 = number1 * number2;
+        int result4 = number1 / number2;
+
+        // logica
+        /**
+         * >
+         * >=
+         * <
+         * <=
+         * ==
+         * !=
+         * && and
+         * || or
+         * !
+         */
+
+
+        // Asignacion
+        /**
+         *  =
+         *  +=
+         *  -=
+         *  /=
+         *  *=
+         * %=
+         */
+
+        // Incremento - Decremento - concatenacion
+
+        // ++
+        // --
+        // +
+    }
+}
